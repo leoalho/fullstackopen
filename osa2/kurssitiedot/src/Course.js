00ffc795ea2cf6ee8eadf16ec7585dba078c22f3
Leo <leo.a.alho@gmail.com>
2022-09-25 10:08:04 +0300

    Exercises 2.1-2.5

---
+++ osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,26 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p><b>Number of exercises {sum}</b></p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part}/>)}     
+  </>
+
+const Course = ({course}) =>{
+
+  const sum = course.parts.reduce((previousValue, currentValue) => {return(previousValue + currentValue.exercises)},0)
+  return(
+  <>
+    <Header course={course.name}/>
+    <Content parts={course.parts}/>
+    <Total sum={sum}/>
+  </>
+)}
+
+export default Course
