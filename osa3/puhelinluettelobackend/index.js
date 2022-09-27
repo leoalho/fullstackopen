d43898b91d5bc2a2d13b6377d8ac895fcecfbf2f
Leo <leo.a.alho@gmail.com>
2022-09-27 17:35:12 +0300

    Exercises 3.1-3.6

---
+++ osa3/puhelinluettelobackend/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let henkilot = [
+      { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+  
+app.get('/api/persons', (req, res) => {
+    res.json(henkilot)
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = Math.floor(Math.random() * 10000)
+    const person = req.body
+    if (henkilot.filter(note => note.name === person.name).length>0){
+        return res.status(400).json({ 
+            error: 'Name must be unique.' 
+        })
+    }
+    if (!person.name){
+        return res.status(400).json({
+            error: 'Name missing'
+        })
+    }
+    if (!person.number){
+        return res.status(400).json({
+            error: 'Number missing'
+        })
+    }
+    person.id = id
+    henkilot = henkilot.concat(person)
+    res.json(person)
+
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const person = henkilot.find(person => person.id === id)
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    henkilot = henkilot.filter(note => note.id !== id)
+    res.status(204).end()
+})
+
+app.get('/info', (req,res) => {
+    res.send(`Phonebook has info for ${henkilot.length} persons<br>${new Date().toUTCString()}`)
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log(new Date().toUTCString())
+})
+
