f5f8eb758b4a9794d873ad3d93b48b15e4aeef98
Leo <leo.a.alho@gmail.com>
2022-11-17 08:04:41 +0200

    Exercise 8.15: Added error catching for edit Author

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const { v1: uuid } = require('uuid')
 
 const config = require('./config')
@@ -165,25 +165,34 @@ const resolvers = {
       let author = await Author.find({name: args.author})
       if (author.length===0){
         const newAuthor = new Author({ name: args.author})
-        newAuthor.save()  
+        const result = await newAuthor.save()
+        var authorid = result._id
+      }else{
+        var authorid = author[0]._id
       }
-      author = await Author.find({name: args.author})
-      const book = new Book({...args, author: author[0]._id})
-      try{
-        await book.save()
-      } catch (error) {
+      const book = new Book({...args, author: authorid})
+      const result = await book.save()
+      .catch(error => {
         throw new UserInputError(error.message, {
           invalidArgs: args,
         })
-      }
+      })
+      await result.populate("author")
       
       return book
     },
     editAuthor: async (root, args) => {
       
       const author = await Author.findOne({ name: args.name })
-      author.born = args.setBornTo
-      await author.save()
+      try{
+        author.born = args.setBornTo
+        const result = await author.save()
+      }catch(error){
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+      return result
     },
     createUser: async (root, args) => {
       const user = new User({ username: args.username })
